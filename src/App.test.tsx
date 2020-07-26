08527d59a22248b59b47b7ceee9f36f03234086d
Harshit Agarwal <haagar@microsoft.com>
2020-07-26 23:10:08 +0530

    Adding jest-axe and a failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('Main app', () => {
+  it('should check for accessibility issues', async () => {
+      const container = render(<App />);
+      const results = await axe(container.baseElement);
+      expect(results).toHaveNoViolations();
+  });
 });
